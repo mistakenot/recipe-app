b75e79a33c0ee27e04338e8744bff843933c1c7a
charlie <the_charlie_daly@hotmail.co.uk>
2019-04-06 14:48:31 +0100

    Init.

---
+++ client/screens/data.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    name: string;
+    iban: string;
+    amount: number;
+}
+
+export const testProductData: Product[] = [
+    {name: "Precision Clean by Oral-B Replacement Heads 4 Pack ", iban:"EAN 4210201848233", amount: 0.8}
+]
